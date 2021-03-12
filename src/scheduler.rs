bc0213955fdaa276235b3caab8439f0416830859
Anthony Pena <anthony.pena@outlook.fr>
2021-03-12 17:23:10 +0100

    feat - load config and app versions on schedule time
    #1

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -4,6 +4,8 @@ use std::time::Duration;
 use chrono::Utc;
 use job_scheduler::{Job,JobScheduler,Schedule};
 
+use crate::config_reader;
+use crate::http_client::get_asset_info;
 use crate::models::OctoDepsState;
 
 pub fn grab_state_periodically(state: &'static RwLock<OctoDepsState>, config_path: String) {
@@ -11,10 +13,32 @@ pub fn grab_state_periodically(state: &'static RwLock<OctoDepsState>, config_pat
         let mut job_scheduler = JobScheduler::new();
         let task = || {
             println!("load config {:?}", config_path);
-            let mut rw_state = state.write().unwrap();
-            rw_state.last_updated_on = Some(Utc::now());
-            println!("Get executed {:?}", rw_state.last_updated_on);
-            // let config = config_reader::read_config(config_path);
+            let config_result = config_reader::read_config(config_path.clone());
+            if config_result.is_ok() {
+                let mut config = config_result.unwrap();
+                for app in config.apps.iter_mut() {
+                    let mut asset_infos = Vec::new();
+                    for asset_url in app.asset_version_urls.iter() {
+                        println!("load asset infos {:?}", asset_url);
+                        let asset_infos_result = get_asset_info(asset_url.clone());
+                        if asset_infos_result.is_ok() {
+                            for asset_info in asset_infos_result.unwrap() {
+                                asset_infos.push(asset_info);
+                            }
+                        } else {
+                            println!("fail to load {:?} {:?}", asset_url, asset_infos_result.err().unwrap());
+                        }
+                    }
+                    app.asset_infos = Some(asset_infos);
+                }
+                {
+                    let mut rw_state = state.write().unwrap();
+                    rw_state.last_updated_on = Some(Utc::now());
+                    rw_state.groups = config.groups;
+                    rw_state.apps = config.apps;
+                    println!("Get executed {:?}", rw_state.last_updated_on);
+                }
+            }
         };
         // every 10 seconds
         // let schedule: Schedule = "1/10 * * * * * *".parse().unwrap();
